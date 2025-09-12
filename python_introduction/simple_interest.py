da3a7538f5202f060de3a435b6e56590e5022f01
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2025-09-12 13:34:51 +0300

    Added an interest calculator task solution

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,11 @@
+"""
+This is a python script to calculate the interest rate on a given
+principle amount, rate of interest, and time
+"""
+principal = 1000
+rate = 0.05
+time = 3
+
+interest = principal * rate * time
+
+print(f"The simple interest is: {interest}")
