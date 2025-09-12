7bacab0ea85970474b8c9943f0a53f2019188b08
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2025-09-12 13:19:03 +0300

    Added task one solution

---
+++ basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+
+operations = {
+            "Addition": lambda x, y: x + y,
+            "Subtraction": lambda x, y: x - y,
+            "Multiplication": lambda x, y: x * y
+        }
+for operand_name, func in operations.items():
+    result = func(number1, number2)
+    print(f"{operand_name} of {number1} and {number2} is {result}")
