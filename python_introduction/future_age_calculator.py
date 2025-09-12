9338b7fd2e6cbfcd241bd56b80c5c66ac3fcad54
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2025-09-12 13:46:15 +0300

    Added solution to the input_Age_calculator task

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,16 @@
+"""
+1. Ask the user for their age through the command line
+2. Deduce the year they were born from their age
+3. Calculate how old the user will be in 2050 from the birth year.
+
+Pseudocode:
+    - Ask the user for their age
+    - Add 27 to their current age
+    - Print the results
+"""
+
+current_age = int(input("Enter your current age: "))
+
+future_age = current_age + 27
+
+print(f"In 2050, you will be {future_age} years old.")
