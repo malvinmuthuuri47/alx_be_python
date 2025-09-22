e30b1212bc90804986ee14122fd55f6527289e10
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2025-09-22 13:46:25 +0300

    Corrected the choose operation prompt for task two

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -18,7 +18,7 @@ The pseudocode is as follows:
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 
-operation = str(input("Which operation do you want to perform? (+, -, *, /): "))
+operation = str(input("Choose the operation (+, -, *, /): "))
 
 if operation == '+':
     result = num1 + num2
