4072f0c7f7881aa3fde11e3b2085f5498c04fb3e
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2025-09-22 14:38:46 +0300

    Corrected case_calculator logic by implementing match case in place of if-else statements

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -20,20 +20,21 @@ num2 = int(input("Enter the second number: "))
 
 operation = str(input("Choose the operation (+, -, *, /): "))
 
-if operation == '+':
-    result = num1 + num2
-    print(f"The result is {result}")
-elif operation == '-':
-    result = num1 - num2
-    print(f"The result is {result}")
-elif operation == '*':
-    result = num1 * num2
-    print(f"The result is {result}")
-elif operation == '/':
-    if num2 == 0:
-        print("Cannot divide by zero")
-    else:
-        result = num1 / num2
+match operation:
+    case '+':
+        result = num1 + num2
         print(f"The result is {result}")
-else:
-    print("Wrong Operation")
+    case '-':
+        result = num1 - num2
+        print(f"The result is {result}")
+    case '*':
+        result = num1 * num2
+        print(f"The result is {result}")
+    case '/':
+        if num2 == 0:
+            print("Cannot divide by zero")
+        else:
+            result = num1 / num2
+            print(f"The result is {result}")
+    case _:
+        print("Wrong Operation")
