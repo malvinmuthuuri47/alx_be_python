faeb00981b39a3a64a3e5c1df80f701f68914092
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2025-09-22 12:17:41 +0300

    Added solution to the weather advice task

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,26 @@
+'''
+This module is used to recommend the clothes a user will wear based on the weather they describe.
+
+The pseudocode is as follows:
+    START
+    1. Prompt the user for weather input
+    2. Check that the input matches the expected prompts:
+        - If the input is ("sunny", "rainy", "cold"), make the following recommendations:
+            -> Sunny -> Wear a t-shirt and sunglasses
+            -> Rainy -> Don't forget your umbrella and a raincoat
+            -> Cold -> Make sure to wear a coat and a scarf
+        - If input doesn't match the expected conditions:
+            -> Print an error message to the standard output
+    END
+'''
+
+user_weather_projection = str(input("What's the weather like today? (sunny/rainy/cold): ")).strip().lower()
+
+if user_weather_projection == "sunny":
+    print("Wear a t-shirt and sunglasses.")
+elif user_weather_projection == "rainy":
+    print("Don't forget your umbrella and a raincoat.")
+elif user_weather_projection == "cold":
+    print("Make sure to wear a warm coat and a scarf.")
+else:
+    print("Sorry, I don't have recommendations for this weather.")
