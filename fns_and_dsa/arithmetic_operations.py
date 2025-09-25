aba8e203d1cc09cdbca26aa19d24be397eac7144
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2025-09-25 14:34:15 +0300

    Replaced logic checking for zerodivision as required

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -17,9 +17,9 @@ def perform_operation(num1, num2, operation):
     elif operation == 'multiply':
         return num1 * num2
     elif operation == 'divide':
-        if num2 != 0:
-            return num1 / num2
-        else:
+        if num2 == 0:
             return 'ZeroDivisionError: division by zero'
+        else:
+            return num1 / num2
     else:
         return 'You entered the wrong operation'
