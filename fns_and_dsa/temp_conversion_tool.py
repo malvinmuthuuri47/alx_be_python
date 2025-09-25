0f073d9efbdcceaebb524404994eccea4632964e
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2025-09-25 15:00:12 +0300

    REfactored the function by correcting the conversion formula to and from celcius and fahrenheit

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -10,7 +10,7 @@ def convert_to_celsius(fahrenheit):
     This function takes temperature in fahrenheit and returns the temperature
     converted to celcius
     """
-    res = fahrenheit * FAHRENHEIT_TO_CELSIUS_FACTOR
+    res = fahrenheit * FAHRENHEIT_TO_CELSIUS_FACTOR - 32
     print(f"{fahrenheit} is {res}25\u00B0 C")
 
 def convert_to_fahrenheit(celsius):
@@ -18,7 +18,7 @@ def convert_to_fahrenheit(celsius):
     This function takes temperature in Celcius and returns the temperature
     converted to fahrenheit
     """
-    res = celsius * CELSIUS_TO_FAHRENHEIT_FACTOR
+    res = celsius * CELSIUS_TO_FAHRENHEIT_FACTOR + 32
     print(f"{celsius} is {res}25\u00B0 F")
 
 def main():
