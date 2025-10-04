400d4f6bd154d10734513cfdf42809055c6a61f2
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2025-10-04 22:38:59 +0300

    corrected typo on the display_balance method to the expected output string

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -52,4 +52,4 @@ class BankAccount:
         This method displays the value in the persisted text file after all
         the operations have been performed
         """
-        print(f"Current balance: ${int(self.account_balance)}")
+        print(f"Current Balance: ${int(self.account_balance)}")
