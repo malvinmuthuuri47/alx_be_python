6dda844898f7de5e4f78db3186d91bbfd1c4f6e8
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2025-10-09 01:45:12 +0300

    Added python magic methods solution

---
+++ oop/book_class.py
@@ -0,0 +1,28 @@
+"""
+This module defines a class and implements specific magic methods to enhance
+its functionality.
+"""
+class Book:
+    def __init__(self, title: str, author: str, year: int) -> None:
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __str__(self) -> str:
+        """
+        A function that returns a string representation of the instance
+        """
+        return f"{self.title} by {self.author}, published in {self.year}"
+
+    def __repr__(self) -> str:
+        """
+        A function that returns an official representation of the instance
+        """
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+
+    def __del__(self) -> None:
+        """
+        A function that's called when the object is deleted or no longer
+        referenced by the program
+        """
+        print(f"Deleting {self.title}")
