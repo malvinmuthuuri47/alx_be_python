69d9037ff599ce69fbb481a1b353248f2223599e
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2025-10-09 02:00:26 +0300

    Added solution to the polymorphism and method overriding task

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,24 @@
+"""
+This module defines OOP concepts Polymorphism and Method overloading
+"""
+
+import math
+
+class Shape:
+    def area(self):
+        raise NotImplementedError("Subclass must define this method")
+
+class Rectangle(Shape):
+    def __init__(self, length: int, width: int) -> None:
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius * self.radius
