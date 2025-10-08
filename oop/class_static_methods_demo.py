a0ef6915e0083a0b2572cdd5d2225022c8754401
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2025-10-09 02:08:02 +0300

    Removed type annotations from the static and class methods

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -6,10 +6,10 @@ class Calculator:
     calculation_type = "Arithmetic Operations"
 
     @staticmethod
-    def add(a: int, b: int) -> int:
+    def add(a, b):
         return a + b
 
     @classmethod
-    def multiply(cls, a: int, b: int) -> int:
+    def multiply(cls, a, b):
         print(f"Calculation type: {cls.calculation_type}")
         return a * b
